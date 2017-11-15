816e00ed8541aeb3e72b897d32adaae8353f0552
Ashish Doneriya <ashishdoneriya@gmail.com>
2017-11-15 18:38:11 +0530

    Added id

--- http-loader-master/api/initiate.php
+++ http-loader-master/api/initiate.php
@@ -12,17 +12,17 @@ $query = "create table if not exists users ( userId int primary key not null aut
 $db->query($query);
 
 // Creating table 'users_tables'
-//public role = none/user/contributor/feeder
+//public role = none/user/contributorRW/contributorR/contributorW
 $query = "create table if not exists users_tables ( userId int not null, tableName varchar(30) not null, displayedTableName varchar(100) not null, fields text not null, publicRole varchar(15) not null, foreign key (userId) references users(userId))";
 $db->query($query);
 
 // Creating table 'guests_permissions'
-//role = administrator / contributor / user
+//role = administrator / contributorRW / contributorR / contributorW / user
 $query = "create table if not exists guest_permissions ( userId int not null, tableName varchar(30) not null, role varchar(30) not null, foreign key (userId) references users(userId))";
 $db->query($query);
 
-// Creating table 'access_requests'
-$query = "create table if not exists write_requests ( userId int not null, tableName varchar(30) not null, fields text not null, foreign key (userId) references users(userId), foreign key (refreeId) references users(userId))";
+// Creating table 'write_requests'
+$query = "create table if not exists write_requests (reqId int primary key not null auto_increment, userId int not null, tableName varchar(30) not null, fields text not null, foreign key (userId) references users(userId), foreign key (refreeId) references users(userId))";
 $db->query($query);
 
 echo "success";
