5d35e2b32dd7e48c95dea3894e8ef6728b8fe64c
Ashish Doneriya <ashishdoneriya@gmail.com>
2017-11-27 14:35:21 +0530

    Changed theme color to #002B36

--- http-loader-master/components/User.vue
+++ http-loader-master/components/User.vue
@@ -1,7 +1,7 @@
 <template>
 	<el-container>
 		<el-header>
-			<el-menu :default-active="'1'" class="el-menu-demo" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+			<el-menu :default-active="'1'" class="el-menu-demo" mode="horizontal" background-color="#002B36" text-color="#fff" active-text-color="#ffd04b">
 				<el-menu-item @click="goToHome()" index="1">
 					<a>Home</a>
 				</el-menu-item>
